47fa2edcdcbe52b20358e4c421792d729e3aa8ce
Alexey Barinov <begoodbebad2012@gmail.com>
2021-06-04 17:01:43 +0300

    Initial Commit

---
+++ ColorizeSwiftUI/ColorizeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizeSwiftUIApp.swift
+//  ColorizeSwiftUI
+//
+//  Created by Mac on 04.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorizeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
